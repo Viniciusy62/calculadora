7d28fbb1b3852e70c7a282e9742942f2b30ad457
Vinicius Andrade <viniciusy62@gmail.com>
2023-07-24 19:28:27 -0300

    created id's HTML  and variables Js

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const num1 = document.querySelector('#num1');
+const num2 = document.querySelector('#num2');
+const num3 = document.querySelector('#num3');
+const num4 = document.querySelector('#num4');
+const num5 = document.querySelector('#num5');
+const num6 = document.querySelector('#num6');
+const num7 = document.querySelector('#num7');
+const num8 = document.querySelector('#num8');
+const num9 = document.querySelector('#num9');
+const num0 = document.querySelector('#num0');
+
+const btCE = document.querySelector('#bt-ce');
+const btC = document.querySelector('#bt-c');
+const btPercentage = document.querySelector('#bt-percentage');
+const btDivider = document.querySelector('#bt-divider');
+const btMultiply = document.querySelector('#bt-multiply');
+const btSubtract = document.querySelector('#bt-subtract');
+const btSum = document.querySelector('#bt-sum');
+const btEqual = document.querySelector('#bt-equal');
+const btSumSubtract = document.querySelector('#bt-sum-subtract');
+const btDot = document.querySelector('#bt-dot');
+
+const screenResult = document.querySelector('#screen-result');
+let screenOperations = document.querySelector('#screen-operations');
+
