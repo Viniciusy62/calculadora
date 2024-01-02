3ad56daa14e3bd2b9a800f4b19a036af5ed56382
Vinicius Andrade <viniciusy62@gmail.com>
2024-01-02 16:37:16 -0300

    update simple

--- js/main.js
+++ js/main.js
@@ -1,25 +1,42 @@
-const numbers = document.querySelectorAll(".numbers");
 const screenResult = document.querySelector("#screen-result");
+let screenOperations = document.querySelector('#screen-operations');
 
 
-numbers.forEach(number => {
-  number.addEventListener("click", () => {
-    const num = number.value
-    screenResult.textContent = num
-  })
-})
+
+const btC = document.querySelector('#bt-c');
+
+const clear = () => {
+  screenOperations.textContent = ""
+}
+
+btC.addEventListener("click", clear)
+// Clear button end
+
 
 
 const btCE = document.querySelector('#bt-ce');
-const btC = document.querySelector('#bt-c');
 const btPercentage = document.querySelector('#bt-percentage');
 const btDivider = document.querySelector('#bt-divider');
 const btMultiply = document.querySelector('#bt-multiply');
 const btSubtract = document.querySelector('#bt-subtract');
 const btSum = document.querySelector('#bt-sum');
-const btEqual = document.querySelector('#bt-equal');
 const btSumSubtract = document.querySelector('#bt-sum-subtract');
 const btDot = document.querySelector('#bt-dot');
 
-let screenOperations = document.querySelector('#screen-operations');
+  const buttons = document.querySelectorAll("button")
+  
+  buttons.forEach(button => {
+    button.addEventListener("click", () => {
+      const buttonValue = button.value
+      screenOperations.textContent += buttonValue
+    })
+  })
+  
+  const btEqual = document.querySelector('#bt-equal');
 
+  const showResult = () => {
+    // const result = screenOperations.textContent
+    // console.log(result)
+  }
+
+  btEqual.addEventListener("click", showResult)
